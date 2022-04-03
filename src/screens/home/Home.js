4746b7e294b638bf22b6b09598d0b8410fc57e2e
Ankit <akpareek4@gmail.com>
2022-04-03 13:26:53 +0530

    added SingleLineImageList below header.

--- src/screens/home/Home.js
+++ src/screens/home/Home.js
@@ -1,15 +1,32 @@
 import React from "react";
+import './Home.css'
 import "../../common/header/Header"
 import Header from "../../common/header/Header.js";
+import SingleLineImageList from "./movieList";
+import moviesData from "../../common/moviesData";
+
 
 class Home extends React.Component{
+
+    constructor(props) {
+        super(props);
+    
+        this.state = { data: moviesData };
+    }
+
     render(){
         return(
-            <div>
-                <Header />
-            </div>
+            
+                <div>
+                    <Header />
+                    <span className="heading">Upcoming Movies</span>
+                    <SingleLineImageList moviesData={this.state.data}/>
+
+                </div>
         );
+
     }
-}
+}  
+
 
 export default Home;
